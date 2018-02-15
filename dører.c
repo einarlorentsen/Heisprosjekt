9d3e363f9ea52471b8aad0d0d3fc02c1d1f4b923
JosteinSchefte <jostein.s.s@hotmail.no>
2018-02-15 17:59:14 +0100

    test

--- dører.c
+++ dører.c
@@ -3,12 +3,12 @@
 
 bool åpen=0;
 
-void åpne() {
+void åpne() {//Åpner og setter status til åpnet
 	elev_set_door_open_lamp(1);
 	åpen = 1;
 }
 
-void lukk() {
+void lukk() {//Lukker og setter status til lukket
 	elev_set_door_open_lamp(0);
 	åpen = 0;
 }
--- heis.h
+++ heis.h
@@ -1,1 +1,5 @@
 #pragma once
+
+void setNåværendeEtasje();
+void kjørStopp();
+void nødstopp();
