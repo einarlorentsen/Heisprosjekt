12830d770a44e1f97155e8f00092df413416c250
JosteinSchefte <jostein.s.s@hotmail.no>
2018-02-22 15:24:02 +0100

    Timer på plass og UML endring

---
+++ timer.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+
+time_h setTimer(int tid) {
+	time_h seconds = time(NULL)+tid;
+	return seconds;
+}
+
+int timerFerdig(time_h seconds){
+	time_h = nåværendeTid;
+	if (seconds - nåværendeTid = 0 || seconds - nåværendeTid < 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
