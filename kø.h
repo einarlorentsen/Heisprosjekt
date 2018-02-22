7fe5037c184817055339a2b944909b96dc207917
Einar Lorentsen <einar.lorentsen@hotmail.no>
2018-02-22 17:00:00 +0100

    Prøver å legge til kø.h

--- kø.h
+++ kø.h
@@ -1,1 +1,9 @@
-#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int sjekkKnappOpp();
+int sjekkKnappNed();
+void heisKø(int heiskø);
+void sjekkStopp(int heiskø, elev_motor_direction_t heisRetning, int etasje);
