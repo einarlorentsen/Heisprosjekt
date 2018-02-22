a2a445fda7fe07cf31e34cdfd2208b4c5ea90886
JosteinSchefte <jostein.s.s@hotmail.no>
2018-02-22 15:45:07 +0100

    småfiks

--- dører.c
+++ dører.c
@@ -3,12 +3,12 @@
 
 bool åpen=0;
 
-void åpne() {//Åpner og setter status til åpnet
+void åpne() {
 	elev_set_door_open_lamp(1);
 	åpen = 1;
 }
 
-void lukk() {//Lukker og setter status til lukket
+void lukk() {
 	elev_set_door_open_lamp(0);
 	åpen = 0;
 }
--- dører.h
+++ dører.h
@@ -1,4 +1,7 @@
 #pragma once
 
+//Åpner og setter status til åpnet
 void åpne();
+
+//Lukker og setter status til lukket
 void lukk();
