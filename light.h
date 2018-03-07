3d291c6f796415a6a9b40516b008cc32ce3fe86d
einarlorentsen <36454861+einarlorentsen@users.noreply.github.com>
2018-03-07 12:15:43 +0100

    Delete light.h

--- light.h
+++
@@ -1,7 +0,0 @@
-#include "elev.h"
-#include "io.h"
-#include "channels.h"
-
-void obstructionLight(); //Setter på obstruksjonslys og stopplys hvis de er trykket inn
-void lightsOn();
-void lightsOff();
