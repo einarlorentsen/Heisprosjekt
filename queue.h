a0e9c62433b94483cd12b80030d3cfc897380b11
einarlorentsen <36454861+einarlorentsen@users.noreply.github.com>
2018-03-07 12:16:22 +0100

    Delete queue.h

--- queue.h
+++
@@ -1,6 +0,0 @@
-#include "elev.h"
-
-int checkFloorButtonUp(); //Sjekker om en av nedknappene er trykket
-int checkFloorButtonDown(); //Sjekker om en av oppknappene er trykket
-void updateElevatorQueue(int elevatorQueue[3][4]); //Lager heiskøen
-void checkStop(int elevatorQueue[3][4], elev_motor_direction_t elevatorDirection, int floor); //Sjekker om vi skal stoppe på en gitt etasje
